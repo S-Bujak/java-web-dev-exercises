6375f92ca56f83fd15143e24dafe819759f419e5
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-07-25 17:51:47 -0400

    completed ch4 exercises

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -2,6 +2,17 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
     public static void main(String[] args) {
+        Student me = new Student();
+        me.setName("Stephanie");
+        me.setStudentId(2);
+        me.setNumberOfCredits(1);
+        me.setGpa(4.0);
         // Instantiate your Student class for part 2 here!
     }
 }
+/*public class Student {
+
+    private String name;
+    private int studentId;
+    private int numberOfCredits = 0;
+    private double gpa = 0.0;*/
