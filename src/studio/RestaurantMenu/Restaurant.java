5862fc63753b07ef32e5d3f4d1fcbc778e8a23e9
S-Bujak <106119256+S-Bujak@users.noreply.github.com>
2022-08-04 15:03:13 -0400

    Unit Testing Exercises

--- src/studio/RestaurantMenu/Restaurant.java
+++ src/studio/RestaurantMenu/Restaurant.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class Restaurant {
     public static void main(String[] args) {
-        ArrayList<MenuItem> myBucket = new ArrayList<>()
+        ArrayList<MenuItem> myBucket = new ArrayList<>();
         MenuItem bakedBrie = new MenuItem("Baked Brie", "Brie baked to gooey goodness and topped with fig jam and pistachio", "Appetizer", 12.99 );
         MenuItem steak = new MenuItem("Steak", "Choice cut cooked to perfection served with garlic mashed potatoes.", "Entree", 29.99);
         MenuItem cheesecake = new MenuItem ("cheesecake", "Rich and delicious on our spiced wafer crust", "Dessert", 9.75);
